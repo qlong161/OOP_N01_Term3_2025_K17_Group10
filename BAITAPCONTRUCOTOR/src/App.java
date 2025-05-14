9873591a333e664974fd205f8267f94aa8d192fc
Nguyễn Hữu Quang Long <23010309@st.phenikaa-uni.edu.vn>
2025-05-14 11:16:48 +0000

    update setter va getter

--- BAITAPCONTRUCOTOR/src/App.java
+++ BAITAPCONTRUCOTOR/src/App.java
@@ -1,6 +1,6 @@
 class App{
     public static void main(String[] args) {
-        Customer customer1 = new Customer("C001", "Alice");
+        Customer customer1 = new Customer("01", "Alice","example@gmail.com");
         customer1.displayInfo();
     }
 }
--- BAITAPCONTRUCOTOR/src/Customer.java
+++ BAITAPCONTRUCOTOR/src/Customer.java
@@ -5,15 +5,25 @@ public class Customer {
     private String name; //null
     private String email; //null
 
+    Customer (String id, String name , String email) {
+    this.id = id;
+    this.name = name;
+    this.email = email;
+    }
 
+    public String getName(){
+        return name;
+    }
 
-    //constructor  
-    //String email
-    
-    Customer(String id, String name ) {
-        this.id = id;     
-        this.name = name; 
-     //   this.email = email; 
+    public void setName(String name){
+        this.name = name;
+    }
+  
+    public String getEmail(){
+        return email;
+    }
+    public void setEmail(String email){
+        this.email = email;
     }
 
     //method
