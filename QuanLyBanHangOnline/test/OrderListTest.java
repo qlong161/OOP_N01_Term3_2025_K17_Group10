f6c38cdd6194d761e6b1fa6d600070e8301e7833
vietlong-05 <23010278@st.phenikaa-uni.edu.vn>
2025-06-11 09:23:58 +0000

    .

--- QuanLyBanHangOnline/test/OrderListTest.java
+++ QuanLyBanHangOnline/test/OrderListTest.java
@@ -6,25 +6,36 @@ public class OrderListTest {
         Customer c2 = new Customer("C002", "Tran Thi B", "b@gmail.com", "Đặt Online");
 
         OrderList orderList = new OrderList();
-        Order o1 = new Order("O001", c1, p1, 2,"đã xử lý"); // Đơn hàng 1
-        Order o2 = new Order("O002", c2, p2, 1,"đã xử lý"); // Đơn hàng 2
-        Order o3 = new Order("O003", c2, p2, 1,"chưa xử lý");
+        Order o1 = new Order("O001", c1, p1, 2, "đã xử lý"); // Đơn hàng 1
+        Order o2 = new Order("O002", c2, p2, 1, "đã xử lý"); // Đơn hàng 2
+        Order o3 = new Order("O003", c2, p2, 1, "chưa xử lý");
+
         orderList.addOrder(o1);
         orderList.addOrder(o2);
-        
-        System.out.println("đơn hàng sau khi thêm:");
+
+        System.out.println("Đơn hàng sau khi thêm:");
         orderList.displayOrderList();
+
         System.out.println("\nCập nhật đơn hàng O001:");
-        orderList.editOrder("O001", c1, p2, 3, "dang xu ly");
-        System.out.println("\nđơn hàng sau khi cập nhật:");
+        orderList.editOrder("O001", c1, p2, 3, "đang xử lý");
+
+        System.out.println("\nĐơn hàng sau khi cập nhật:");
         orderList.displayOrderList();
 
         System.out.println("\nXoá đơn hàng O002:");
         orderList.deleteOrder("O002");
+
         orderList.displayOrderList();
 
-        //Hien thi tat ca thong tin cua o1
+        System.out.println("\nThông tin đơn hàng O001:");
         o1.displayOrder();
-        o1.getProcessedOrderByDate();
+       // o1.getProcessedOrderByDate();
+
+        // Test hàm lọc đơn hàng theo ngày nếu có
+        // System.out.println(orderList.getProcessedOrderByDate("2025-06-10"));
+    }
+
+    public static void main(String[] args) {
+        test(); // Gọi test() từ main
     }
 }
