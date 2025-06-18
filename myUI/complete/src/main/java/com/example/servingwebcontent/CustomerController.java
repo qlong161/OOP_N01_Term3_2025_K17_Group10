b4f07506cf9ce2c6207bcb5badd1a0c75da3f591
Nguyễn Hữu Quang Long <23010309@st.phenikaa-uni.edu.vn>
2025-06-18 10:42:37 +0000

    test

---
+++ myUI/complete/src/main/java/com/example/servingwebcontent/CustomerController.java
@@ -0,0 +1,36 @@
+
+
+
+package com.example.servingwebcontent;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.example.servingwebcontent.database.CustomerAiven;
+
+import java.util.ArrayList;
+
+
+//controller to song list
+@Controller
+public class CustomerController {
+    @GetMapping("/customerlist")
+
+    public String songlist(Model model){
+
+        ArrayList<Customer> cusList = new ArrayList<Customer>();
+        CustomerAiven sa = new CustomerAiven();
+        cusList = sa.customerAivenList();
+        model.addAttribute("ListOfCustomer", cusList);
+
+        //data to View is ${ListOfSong}
+        
+       //return view is songList
+        return "customerlist";
+
+    }
+
+    
+}
+
---
+++ myUI/complete/src/main/java/com/example/servingwebcontent/database/CustomerAiven.java
@@ -0,0 +1,98 @@
+package com.example.servingwebcontent.database;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+//import java.util.List;
+
+import com.example.servingwebcontent.Customer;
+
+
+
+
+
+
+public class CustomerAiven {
+
+    public CustomerAiven(){}
+
+    /*
+     * to do
+     * mapping database data to Model Song
+     */
+
+    ArrayList<Customer> items = new ArrayList<Customer>(); 
+  
+
+    /**
+     * @return
+     */
+    public ArrayList<Customer> customerAivenList() {
+      
+        Connection conn = null;
+        try {
+           
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            
+            conn = DriverManager.getConnection(
+                    "jdbc:mysql://avnadmin:AVNS_qcy9-3v56k3F_fag4J5@mysql-2a826328-st-cf76.c.aivencloud.com:24247/defaultdb?ssl-mode=REQUIRED",
+                    "sqluser", "password");
+            Statement sta = conn.createStatement();
+
+            ResultSet setdata = sta.executeQuery("select * from Customer limit 10");
+            int index =0;
+            int columnCount = setdata.getMetaData().getColumnCount();
+             System.out.println("column #"+columnCount);
+   
+          
+
+            while (setdata.next()) {
+                Customer cust = new Customer();
+               
+              
+                String id = setdata.getString("id");
+           
+                String name = setdata.getString("name");
+          
+                String email = setdata.getString("email");
+
+                String type = setdata.getString("type");
+
+                System.out.println("Song AIVEN TEST:");
+                System.out.println(id + " " + name + " " + email);
+
+                cust.setId(id);
+                cust.setName(name);
+                cust.setEmail(email);
+                cust.setType(type);
+
+
+                System.out.println("Get SONG in song Aiven");
+                System.out.println(cust.getName());
+                System.out.println(index);
+                
+
+        
+            items.add(cust);
+       }
+
+            setdata.close();
+            sta.close();
+            conn.close();
+           
+        } 
+        catch (Exception e) {
+            System.out.println("Error in database connecion");
+            System.out.println(e);
+            e.printStackTrace();
+        }
+
+        
+        return items;
+
+    }
+    
+}
